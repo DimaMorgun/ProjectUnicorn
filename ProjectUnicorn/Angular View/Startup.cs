f38bcc4b652548e55f54789855c85e3e7667f204
Dmytro Morhun <DimaMorgun97@gmail.com>
2018-09-03 17:30:52 +0300

    Set up angular/desktop application.

--- ProjectUnicorn/Angular View/Startup.cs
+++ ProjectUnicorn/Angular View/Startup.cs
@@ -1,42 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Angular_View
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseMvc();
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+      Bootstrap();
+    }
+
+    public async void Bootstrap()
+    {
+      var options = new BrowserWindowOptions
+      {
+        WebPreferences = new WebPreferences
+        {
+          WebSecurity = false
+        }
+      };
+
+      await Electron.WindowManager.CreateWindowAsync(options);
+    }
+  }
 }
